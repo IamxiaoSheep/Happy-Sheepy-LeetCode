67ebec24f57439adfd3e0a7ca312fe357bd12b15
I am Sheep <jorgexiaoyang@gmail.com>
2022-05-14 13:15:53 -0700

    Cycle Finding Algorithm

--- 21-Merge Two Sorted Lists.java
+++ 21-Merge Two Sorted Lists.java
@@ -8,6 +8,21 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//Floyd's Cycle Finding Algorithm
+//round 1
+// 1-2-3-> goes back to 1
+// s-f
+//check if f is null or f.next is null, then there must be no cycle because if there is then f would go back to another node.
+//round 2
+//1-2-3
+//f-s
+// there's a cycle that f went into then we keep going
+//round 3
+//1-2-3
+//0-0-s/f 
+//they meet at certain node so there's a cycle and we break
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
        ListNode head = new ListNode(0);
--- 83-Remove Duplicates from Sorted List.java
+++ 83-Remove Duplicates from Sorted List.java
@@ -1,3 +1,17 @@
+
+//Floyd's Cycle Finding Algorithm
+//round 1
+// 1-2-3-> goes back to 1
+// s-f
+//check if f is null or f.next is null, then there must be no cycle because if there is then f would go back to another node.
+//round 2
+//1-2-3
+//f-s
+// there's a cycle that f went into then we keep going
+//round 3
+//1-2-3
+//0-0-s/f 
+//they meet at certain node so there's a cycle and we break
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
     if(head == null || head.next == null) {
