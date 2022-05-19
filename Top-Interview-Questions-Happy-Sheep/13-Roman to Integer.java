817289480894a77d4a7f3deac8124c8c302d23dd
I am Sheep <jorgexiaoyang@gmail.com>
2022-05-19 15:43:01 -0700

    13

---
+++ Top-Interview-Questions-Happy-Sheep/13-Roman to Integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<String, Integer> values = new HashMap();
+        values.put("I", 1);
+        values.put("V", 5);
+        values.put("X", 10);
+        values.put("L", 50);
+        values.put("C", 100);
+        values.put("D", 500);
+        values.put("M", 1000);
+        int sum = 0;
+        int i =0;
+        while(i<s.length()) {
+            String currentSymbol = s.substring(i,i+1);
+            int currentValue = values.get(currentSymbol);
+            int nextValue = 0;
+            if(i+1 <s.length()) {
+                String nextSymbol = s.substring(i+1,i+2);
+                nextValue = values.get(nextSymbol);
+            }
+            if(currentValue < nextValue) {
+                sum+= (nextValue-currentValue);
+                i+=2;
+            }
+            else {
+                sum+=currentValue;
+                i+=1;
+            }
+        }
+        return sum;
+        
+}
+}
